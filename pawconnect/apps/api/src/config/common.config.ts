054a9be0736cf524cdb181d21fd0a83b088c7434
블드 <hjkim4177@gmail.com>
2026-07-13 01:19:11 +0900

    fix: common config 수정

--- pawconnect/apps/api/src/config/common.config.ts
+++ pawconnect/apps/api/src/config/common.config.ts
@@ -2,8 +2,8 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('common', () => ({
     domain: process.env.DOMAIN,
-    port: process.env.PORT || 3001,
+    port: Number(process.env.PORT),
     
     webDomain: process.env.WEB_DOMAIN,
-    webPort: process.env.WEB_PORT || 3000,
+    webPort: Number(process.env.WEB_PORT),
 }));
