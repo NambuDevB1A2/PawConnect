e8afd1016bef6761e8ebbeb60270002196526898
블드 <hjkim4177@gmail.com>
2026-07-12 22:34:08 +0900

    feat: register dto 수정

--- pawconnect/apps/api/src/auth/dto/register-auth.dto.ts
+++ pawconnect/apps/api/src/auth/dto/register-auth.dto.ts
@@ -1,2 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Role } from "@prisma/client";
+import { IsEnum, IsString, Length } from "class-validator";
+
 export class RegisterAuthDto {
+    @ApiProperty({ example: "example@email.com" })
+    @IsString()
+    email: string;
+
+    @ApiProperty({ example: "example@email.com" })
+    @IsString()
+    @Length(6, 30)
+    password: string;
+
+    @ApiProperty({ example: "유저1234" })
+    @IsString()
+    @Length(2, 16)
+    nickname: string;
+
+    @ApiProperty({ example: Role.USER })
+    @IsEnum(Role)
+    role: Role;
+
+    // TODO : 이미지 추가
+    // imgProfile: string;
 }
