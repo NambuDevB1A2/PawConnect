e42d45548699ed12acc9a134c5132f652076cd09
블드 <hjkim4177@gmail.com>
2026-07-15 14:14:02 +0900

    feat: iconButton disabled 상태 추가

--- pawconnect/apps/web/src/components/common/Icon.tsx
+++ pawconnect/apps/web/src/components/common/Icon.tsx
@@ -1,11 +1,11 @@
 import { HTMLAttributes } from "react";
 import styles from "@/styles/common/icon.module.css"
 
-type IconVariant = "outlined" | "rounded" | "sharp";
-type IconSize = "badge" | "input" | "default" | "status" | "hero";
-type IconWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700;
-type IconGrade = -25 | 0 | 200;
-type IconColor = "defaultColor" | "primary" | "secondary" | "disabled" | "success" | "warning" | "error";
+export type IconVariant = "outlined" | "rounded" | "sharp";
+export type IconSize = "badge" | "input" | "default" | "status" | "hero";
+export type IconWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700;
+export type IconGrade = -25 | 0 | 200;
+export type IconColor = "defaultColor" | "primary" | "secondary" | "disabled" | "success" | "warning" | "error" | "trueColor" | "falseColor";
 
 interface IconProps extends HTMLAttributes<HTMLSpanElement> {
     name: string; // material sumbols 아이콘 이름
---
+++ pawconnect/apps/web/src/components/common/Input.tsx
@@ -0,0 +1,10 @@
+export default function Input() {
+    return (
+        <span>
+            <p></p>
+            <input>
+            </input>
+            <p></p>
+        </span>
+    );
+}
