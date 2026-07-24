93a419005238b0bb5403845586089859de989340
블드 <hjkim4177@gmail.com>
2026-07-24 22:23:22 +0900

    feat: 게시글 목록 조회, 상세 조회 API 추가

---
+++ pawconnect/apps/api/src/petposts/petpost.select.ts
@@ -0,0 +1,42 @@
+import { Prisma } from "@prisma/client";
+import { USER_SELECT } from '@/users/user.select';
+
+export const PETPOST_SELECT = 
+    Prisma.validator<Prisma.PetPostSelect>()
+    ({
+        id: true,
+        title: true,
+        author: {
+            select: USER_SELECT,
+        },
+        updatedAt: true,
+    });
+
+export const PETPOST_DETAIL_SELECT = 
+    Prisma.validator<Prisma.PetPostSelect>()
+    ({
+        id: true,
+        title: true,
+        content: true,
+        author: {
+            select: USER_SELECT,
+        },
+        updatedAt: true,
+    });
+
+export const PETPOST_ORDERBY = {
+    OLDEST: Prisma.validator<Prisma.PetPostOrderByWithRelationInput>()
+    ({
+        createdAt: 'asc',
+    }),
+
+    NEWEST: Prisma.validator<Prisma.PetPostOrderByWithRelationInput>()
+    ({
+        createdAt: 'desc',
+    }),
+
+    UPDATED: Prisma.validator<Prisma.PetPostOrderByWithRelationInput>()
+    ({
+        updatedAt: 'desc',
+    }),
+};
--- pawconnect/apps/api/src/petposts/petposts.controller.ts
+++ pawconnect/apps/api/src/petposts/petposts.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Public } from '@/auth/decorators/public.decorator';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/auth/guards/role.guard';
+import { QueryPaginationDto } from '@/common/dto/query-pagination.dto';
+import { PetpostsService } from '@/petposts/petposts.service';
+import { Controller, Get, Param, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Pet Post')
 @Controller('petposts')
-export class PetpostsController {}
+@UseGuards(JwtAuthGuard, RolesGuard)
+@ApiBearerAuth()
+export class PetpostsController {
+    constructor (private readonly petpostsService: PetpostsService) {}
+
+    // 게시글 목록 조회
+    @ApiOperation({ summary: "보호소 목록 조회" })
+    @Public()
+    @Get()
+    getPetposts(@Query() pagination: QueryPaginationDto) {
+        return this.petpostsService.getPetposts(pagination);
+    }
+
+    // 게시글 상세 조회
+    @ApiOperation({ summary: "게시글 상세 조회" })
+    @Public()
+    @Get(':id')
+    getPetpost(@Param('id', ParseIntPipe) id: number) {
+        return this.petpostsService.getPetpost(id);
+    }
+}
