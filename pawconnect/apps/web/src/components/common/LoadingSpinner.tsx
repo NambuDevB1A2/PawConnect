2199d292debfc3f1490bc31c90ac7dfba8918f80
BlueDDragon <hjkim4177@gmail.com>
2026-07-29 17:54:00 +0900

    fix: 오타 수정

--- pawconnect/apps/web/src/components/common/LoadingSpinner.tsx
+++ pawconnect/apps/web/src/components/common/LoadingSpinner.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/common/LoadingSpiner.module.css";
+import styles from "@/styles/common/LoadingSpinner.module.css"
 
 type LoadingSpinnerSize = "xlarge" | "large" | "medium" | "small";
 
