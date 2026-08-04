16dd034d540d39a58058036d9f0dbb8a4ce5716e
arim44 <hongarim44@gmail.com>
2026-08-04 22:19:20 +0900

    wip: 내보호송 동물목록조회 프론트 작업중

---
+++ pawconnect/apps/web/src/services/shelters/get-shelter-myanimals.server.ts
@@ -0,0 +1,7 @@
+import { fetchServer } from "../fetch/fetch.server";
+
+// export const getMyAnimals = async (params?: {
+//     page?: number;
+//     limit?: number;
+//     status?: string;
+// }) => {const {data} = await fetchServer.get("")}
