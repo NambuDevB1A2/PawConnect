19ff89acaa9023cfe46708c9bbc7490c280dd6d0
블드 <hjkim4177@gmail.com>
2026-08-04 23:34:14 +0900

    refactor: 반응형 구조 2차 구조 수정

--- pawconnect/apps/web/src/app/pawlab/pawti/test/page.tsx
+++ pawconnect/apps/web/src/app/pawlab/pawti/test/page.tsx
@@ -1,11 +1,9 @@
-import Typography from "@/components/common/Typography";
 import PawTITest from "@/components/pawti/PawtiTest";
 
 // 질문 진행 페이지
 export default function Page() {
     return (
         <div>
-            <Typography variant="title">PaWTI</Typography>
             <PawTITest />
         </div>
     );
